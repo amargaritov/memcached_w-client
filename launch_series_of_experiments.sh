8e46925202802e340b55720f5029f7d509c9078a
Artemiy Margaritov <artemiy.margaritov@gmail.com>
2021-06-20 23:55:11 +0000

    improve launch_series_of_experiments.sh

--- launch_series_of_experiments.sh
+++ launch_series_of_experiments.sh
@@ -1,1 +1,7 @@
-for i in {1..10}; do ./run_study.sh 8 4; mv warm_time studies/4KB/THP/warm_time_$i; mv client_time studies/4KB/THP/client_time_$i; sleep 20; done
+mkdir -p studies/4KB/{THP,noTHP}
+
+./disable_thp_no_drop.sh
+conf=noTHP; for i in {1..10}; do ./run_study.sh 8 4; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; sleep 20; done
+
+./enable_thp_no_drop.sh
+conf=THP; for i in {1..10}; do ./run_study.sh 8 4; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; sleep 20; done
