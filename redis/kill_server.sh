a4591ea2cc8c5356d06ef663f6c4a0fb59cc415e
Ubuntu <ubuntu@ip-172-31-41-224.us-west-2.compute.internal>
2021-07-02 16:06:03 +0000

    add remote scripts for Redis

--- redis/kill_server.sh
+++ redis/kill_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-ps aux | grep redis- | awk '{print $2}' | sudo kill -9 
+ps aux | grep redis- | awk '{print $2}' | sudo xargs kill -9 
 
-sleep 30
+#sleep 30
--- redis/start_server.sh
+++ redis/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+CUR_DIR=$(pwd)
 pushd /disk/memcached_w-client/redis
 redis-server redis.conf > $CUR_DIR/server.log 2>&1 & pid=$! 
 taskset -p -c $SERVER_CORES $pid
