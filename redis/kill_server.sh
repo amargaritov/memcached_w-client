23e7fe36f07976b1ac29d9e30ced5c669491c20d
Ubuntu <ubuntu@ip-172-31-20-209.us-west-2.compute.internal>
2021-07-03 14:46:13 +0000

    up

--- redis/kill_server.sh
+++ redis/kill_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
 ps aux | grep redis- | awk '{print $2}' | sudo xargs kill -9 
+sudo service redis stop
 
 #sleep 30
--- redis/launch_series_of_experiments.sh
+++ redis/launch_series_of_experiments.sh
@@ -1,7 +1,7 @@
 mkdir -p studies/4KB/{THP,noTHP}
 
 ./disable_thp_no_drop.sh
-conf=noTHP; for i in {1..3}; do ./run_study.sh 8 1; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; mv outputRun.txt ./studies/4KB/$conf/outputRun_$i; sleep 10; done
+conf=noTHP; for i in {1..3}; do ./run_study.sh 8 1; mv resmem.log studies/4KB/$conf/resmem.log_$i; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; mv outputRun.txt ./studies/4KB/$conf/outputRun_$i; sleep 10; done
 
 ./enable_thp_no_drop.sh
-conf=THP; for i in {1..3}; do ./run_study.sh 8 1; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; mv outputRun.txt ./studies/4KB/$conf/outputRun_$i; sleep 10; done
+conf=THP; for i in {1..3}; do ./run_study.sh 8 1; mv resmem.log studies/4KB/$conf/resmem.log_$i; mv warm_time studies/4KB/$conf/warm_time_$i; mv client_time studies/4KB/$conf/client_time_$i; mv outputRun.txt ./studies/4KB/$conf/outputRun_$i; sleep 10; done
